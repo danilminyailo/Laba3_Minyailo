c369504eb1274ddf1b2f73ccd79b49aaac766f49
Danil <danilkrava@ukr.net>
2020-04-07 18:28:31 +0300

    laba3

---
+++ laba3_1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba3
+{
+    class Program
+    {/*Дано список з чисел та індекс елемента в списку k. 
+        Видаліть зі списку елемент з індексом k, 
+        зсунувши вгору всі елементи, що стоять нижче елемента з індексом k*/
+        static List<int> listofint = new List<int>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                listofint.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            Console.WriteLine("Enter index of your number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = n; i >=0; i--)
+            {
+                if (i == 0) { }
+
+                else
+                {
+                    int t = listofint[i];
+                    listofint[i] = listofint[i-1];
+                    listofint[i-1] = t;
+                }
+            }
+            foreach (var i in listofint)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine(new string('-',30));
+            listofint.RemoveAt(0);
+            foreach (var i in listofint)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+/*
+ 45
+ 56
+ 67
+ 78
+ 23
+ 45
+ 25
+ 48*/
---
+++ laba3_2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba3_2
+{
+    class Program
+    {
+        /*Дано список та словник. 
+         Створити новий словник, в якому ключами будуть значення списку, 
+         а значеннями ключів — елементи словника*/
+        static List<int> listofint = new List<int>();
+        static Dictionary<int, string> dict = new Dictionary<int, string>();
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 6; i++)
+            {
+                listofint.Add(i + rnd.Next(0,15));
+            }
+            Console.WriteLine("Write your number of words");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Write your words:");
+            for (int i = 0; i < n; i++)
+            {
+                dict.Add(i+rnd.Next(15,20), Console.ReadLine());
+            }
+            Console.WriteLine(new string('-',30));
+            Console.WriteLine("List:");
+            foreach (int  i in listofint)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine(new string('-', 30));
+            Console.WriteLine("Dict:");
+            foreach (var d in dict)
+            {
+                Console.WriteLine($"key: {d.Key} value: {d.Value}");
+            }
+            Dictionary<int, string> keyValuePairs = new Dictionary<int, string>();
+            List<string> strs = new List<string>();
+            foreach (string d in dict.Values)
+            {
+                strs.Add(d);
+            }
+            for (int i = 0; i < Math.Min(listofint.Count,dict.Count); i++)
+            {
+                keyValuePairs.Add(listofint[i], strs[i]);
+            }
+            Console.WriteLine(new string('-', 30));
+            Console.WriteLine("NewDict:");
+            foreach (var d in keyValuePairs)
+            {
+                Console.WriteLine($"key: {d.Key} value: {d.Value}");
+            }
+        }
+    }
+}
